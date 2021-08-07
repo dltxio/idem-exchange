f2b48babf1ad5c626cee92d41b1bec4450244342
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-08-07 19:05:11 +1000

    Begin building

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <form>
+      <div className="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div className="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+      </div>
+      <div className="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>;
 }
 
 export default App;
